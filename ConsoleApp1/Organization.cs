b2ef43084d12e060ed61a83262c1b61c3d48c09c
Paata Kurdadze <paata2016@gmail.com>
2022-04-13 00:51:59 +0400

    Alfa v1.0

--- ConsoleApp1/Organization.cs
+++ ConsoleApp1/Organization.cs
@@ -3,8 +3,11 @@
     public class Organization
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        public string? Name { get; set; }
+
         public int? ParentId { get; set; }
-        public List<Organization> Organizations { get; set; }
+
+        public List<Organization>? Organizations { get; set; }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,30 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp1;
+﻿using ConsoleApp1;
 
-Console.WriteLine("Hello, World!");
 var organizations = new List<Organization>()
 {
+    new Organization{Id = 7, Name ="AB", ParentId = 1},
     new Organization
     {
         Id = 1,
-        Name ="a",
+        Name ="A"
     },
     new Organization
     {
         Id = 2,
         Name ="B",
-        ParentId = 1,
+        ParentId = 1
     },
     new Organization
     {
         Id = 3,
-        Name ="a",
-        ParentId = 2,
+        Name ="C",
+        ParentId = 2
     },
     new Organization
     {
         Id = 4,
-        Name ="B",
-        ParentId = 1,
-    }
+        Name ="D",
+        ParentId = 1
+    },
+     new Organization { Id = 5, Name ="AA"},
+     new Organization { Id = 6, Name ="BB", ParentId = 5}
 };
+
+
+static List<Organization> GetEmbeddedObjects(List<Organization> organizations)
+{
+    List<Organization> result = new();
+
+    foreach (var item in organizations)
+    {
+        if (item.ParentId != null) continue;
+
+        Organization embeddedObjects = new()
+        {
+            Id = item.Id,
+            Name = item.Name
+        };
+
+        organizations.RemoveAt(item.Id);
+
+        embeddedObjects.Organizations = FindChildren(item.Id, ref organizations);
+    }
+
+    return null;
+}
+
+
+static List<Organization> FindChildren(int Id, ref List<Organization> organizations)
+{
+    List<Organization> temp = new();
+
+    foreach (var item in organizations)
+    {
+        if (item.ParentId == Id)
+        {
+            organizations.RemoveAt(item.Id);
+            item.Organizations = FindChildren(item.Id, ref organizations);
+            temp.Add(item);
+        }
+    }
+
+    return temp;
+}
