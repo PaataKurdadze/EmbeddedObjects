2c6f7e8649263d61d07363a33bc41f85a3674ea6
Paata Kurdadze <paata2016@gmail.com>
2022-04-12 18:24:13 +0400

    Add project files.

---
+++ ConsoleApp1/Organization.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+        public List<Organization> Organizations { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+var organizations = new List<Organization>()
+{
+    new Organization
+    {
+        Id = 1,
+        Name ="a",
+    },
+    new Organization
+    {
+        Id = 2,
+        Name ="B",
+        ParentId = 1,
+    },
+    new Organization
+    {
+        Id = 3,
+        Name ="a",
+        ParentId = 2,
+    },
+    new Organization
+    {
+        Id = 4,
+        Name ="B",
+        ParentId = 1,
+    }
+};
